61bad87d376f2ae4c709f6769b4d923a74531bf2
Janus <yucianhuang@gmail.com>
2026-09-12 02:30:27 +0800

    Basic state change store and recall in AppStorage

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    // MARK: - PROPERTY
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Onboarding")
+                .font(.largeTitle)
+            
+            Button(action: {
+                //action - state changes
+                isOnboardingViewActive = false
+            }) {
+                Text("Start")
+            }
+        } //: VStacl
     }
 }
 
+// MARK: - REVIEW
 #Preview {
     OnboardingView()
 }

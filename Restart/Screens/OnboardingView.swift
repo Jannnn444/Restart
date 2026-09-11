7dc0a0049b96dfcf35f4f17a000aec56a92bdbcf
Janus <yucianhuang@gmail.com>
2026-09-12 03:10:20 +0800

    OnboardingView - Finish State Changes

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -57,7 +57,7 @@ struct OnboardingView: View {
                 
                 Spacer()
                 
-                // MARK: - FOOTER
+                // MARK: - FOOTER (BUTTON)
                 
                 ZStack {
                     // PARTS OF THE CUSTOM BUTTON
@@ -70,6 +70,12 @@ struct OnboardingView: View {
                         .padding(8)
                     // 2. CALL-TO-ACTION (STATIC)
                     
+                    Text("Get Starting")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 28)
+                    
                     // 3. CAPSULE RED PART (DYNAMIC WIDTH)
                     HStack{
                         Capsule()
@@ -81,18 +87,23 @@ struct OnboardingView: View {
                     
                     // 4. CIRCLE (DRAGGABLE)
                     HStack {
-                    ZStack {
-                        Circle()
-                            .fill(Color("ColorRed"))
-                        Circle()
-                            .fill(.black.opacity(0.15))
-                            .padding(8) // 8 is smaller than the default padding area
-                        Image(systemName: "chevron.right.2")
-                            .font(.system(size: 24, weight: .bold))
-                    } // ZSTACK
-                    .foregroundColor(.white)
-                    .frame(width: 80, height: 80, alignment: .center)
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8) // 8 is smaller than the default padding area
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        } // ZSTACK
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
                         Spacer() // right side push to left when its hstack
+                        
                     } // HSTACK
                     
                 } //: FOOTER

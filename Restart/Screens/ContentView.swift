7e4d65cdae4c517b289024e3c4fbea94ad9ff8e6
Janus <yucianhuang@gmail.com>
2026-09-12 02:06:13 +0800

    Launch screen and AppStorage

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Janus 333 on 2026/9/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Janus 333 on 2026/9/12.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}

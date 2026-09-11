02e51f7655820cd379070adc3b8e9b75e7463dca
Janus <yucianhuang@gmail.com>
2026-09-12 03:25:18 +0800

    HomeView - Glass Effect Button

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -14,15 +14,43 @@ struct HomeView: View {
     // MARK: - BODY
     var body: some View {
         VStack(spacing:20) {
-            Text("Home")
-                .font(.largeTitle)
+            // MARK: - HEADER
+            Image("character-2")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            // MARK: - CENTER
+            
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // MARK: - FOOTER
+            
+            Spacer()
             
             Button(action: {
                 //action
                 isOnboardingViewActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
                 Text("Restart")
-            }
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            } //: BUTTON (old way vs glass effect)
+            .padding()
+            .glassEffect() //new
+
+//            .buttonStyle(.borderedProminent)
+//            .buttonBorderShape(.capsule)
+//            .controlSize(.large)
+            
         } // :VSTACK
     }
 }

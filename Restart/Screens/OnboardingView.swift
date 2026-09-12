e8b5c52ce74e3a58946e301f8973d8772a62a285
Janus <yucianhuang@gmail.com>
2026-09-12 23:29:39 +0800

    add rotation effect

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -56,7 +56,8 @@ struct OnboardingView: View {
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
-                        .offset(x: imageOffset.width * 1.2, y:0) //x left&right
+                        .offset(x: imageOffset.width * 1.2, y: 0) //x left&right
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
                         .gesture(
                             DragGesture()
                                 .onChanged { gesture in
